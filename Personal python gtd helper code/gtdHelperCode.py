f6a3179838591506ec660ad52df2dd00aaec1200
Conrad Hansen-Quartey <Chansenq@gmail.com>
2021-04-12 10:20:55 -0400

    Add files via upload
    Updates to code to fix some bugs. Yes, you may notice some C++ artifacts since I originally wrote this in C++, but decided to translate it into Python.

--- Personal python gtd helper code/gtdHelperCode.py
+++ Personal python gtd helper code/gtdHelperCode.py
@@ -51,6 +51,15 @@ def make_calEvent(theName, theDayOfWeek, theStartTime, theEndTime, theRepeat):
     calEvent = CalEvent(theName, theDayOfWeek, theStartTime, theEndTime, theRepeat)
     return calEvent
   
+def stringifyCalEvent(aCalEvent):
+    line1 = "name: " + aCalEvent.theName + "\n"
+    line2 = "day of week: " + aCalEvent.theDayOfWeek + "\n"
+    line3 = "start time: " + str(aCalEvent.theStartTime) + "\n"
+    line4 = "end time: " + str(aCalEvent.theEndTime) + "\n"
+    line5 = "repeating event?: " + str(aCalEvent.theRepeat) + "\n"
+    
+    return line1 + line2 + line3 + line4 + line5
+
 
  
 def elapsedTimeBetweenTwoDates(firstTime, secondTime):
@@ -76,11 +85,12 @@ def splitIntoStringList(theStr, charSplitter):
         endPos = theStr.find(charSplitter, startPos)
         if (endPos != -1):
         
-            theList.push_back(theStr.substr(startPos, endPos))
+            theList.append(theStr[startPos : endPos+1])
             startPos = endPos + 1
 
     # end of while loop, last step is to return from function
 
+    print("shown following for testing purposes", theList[2], "\n\n")
     return theList;
     
     
@@ -98,11 +108,56 @@ def automaticEventCreation(inputStr):
     beginTime = 0
     endTime = 0
     
-    name = stringList.at(0)
-    firstDateToSplit = stringList.at(1)
-    secondDateToSplit = stringList.at(2)
+    name = stringList[0]
+    firstDateToSplit = stringList[1]
+    secondDateToSplit = stringList[2]
     
-    testCalEvent = CalEvent(name, lowerCaseString(theDay), beginTime, endTime, false)
+    #print("shown following for testing purposes", inputStr, "\n\n")
+    
+    if(stringList.len() > 0):
+    {
+        if (foundInText(firstDateToSplit, "2021"))
+        {
+            firstTimeSplitPos = firstDateToSplit.find("2021");
+            theDay = firstDateToSplit.substr(0, firstTimeSplitPos - 1);
+            stringifiedBeginTime = firstDateToSplit.substr(firstTimeSplitPos);
+
+            beginTime = std::stoi(stringifiedBeginTime);
+        }
+        else
+        {
+            # I’m not processing dates before Jan 1 2021. Too hard.
+        }
+
+
+        if (foundInText(secondDateToSplit, "2021")) {
+            secondTimeSplitPos = firstDateToSplit.find("2021");
+            #  theDay = secondDateToSplit.substr(0, secondTimeSplitPos-1);
+            stringifiedEndTime = secondDateToSplit.substr(secondTimeSplitPos);
+
+            endTime = std::stoi(stringifiedEndTime);
+        }
+        else
+        {
+            # I’m not processing dates before Jan 1 2021. Too hard.
+        }
+
+        # call function grabDay in here to grab day of week and store in day
+        #Following C++ code concerning time is limited to C++20, which I am having trouble to download
+        #- receive strings and transform them into date objects
+        #- determine the difference in days between two date objects
+        #- determine the day of the week of a particular date
+
+
+        # call function grabTime in here  to grab time and store in beginTime or endTime
+
+        # Function grab time makes two substrings (one on each side of the colon) and
+        # converts those substrings into size_t (using int to size_t casting if necessary)
+        # before doing the math to store as a 3 or 4 dig number representing time
+        # (like how you did it for app inventor)
+     }
+    
+    testCalEvent = CalEvent(name, theDay.lower(), beginTime, endTime, False)
     return testCalEvent;
 
 
@@ -123,32 +178,55 @@ def autoConverter():
 
     fin = "inputFiles/" + fin
 
-    # open input file
+    # open input file to read content
     infile = open(fin, 'r')
     fileContent = infile.readlines()
     
     listOfLines = []
     for line in fileContent:
-        if(line[-1] == ‘\n’):
+        if(line[-1] == '\n'):
             listOfLines.append(line[:-1])
         else:
             listOfLines.append(line)
+            
+    
+    counter = 0
     
     for line in listOfLines:
-        if(foundInText(line, ',') and !foundInText(lower(line), "summary")):
+        if(foundInText(line, ',') and not foundInText(line.lower(), "summary")):
         
             anEvent = automaticEventCreation(line)
+            
+            #print("Event #", str(counter), ": \n", stringifyCalEvent(anEvent), "\n\n")
 
-            autoAddedCalEvents.push_back(anEvent)
-
-    closeInputFile(infile);
-
-
-
-
-    
-a = input("enter first time: ")
-b = input("enter second time: ")
+            autoAddedCalEvents.append(anEvent)
+            
+            counter = counter + 1
 
 
-elapsedTimeBetweenTwoDates(a, b)
+
+def main():
+    #myStr = "";
+    allCalEvents = []
+        
+    anInput = input("Select 1 for the manual event scanning or 2 for the automatic event scanning): ")
+
+    while(anInput != "1" and anInput != "2"):
+        anInput = input("Select a valid input! (1 for the manual event scanning or 2 for the automatic event scanning): ")
+
+    if(anInput == "1"):
+        theCondition = True
+        while (theCondition):
+            allCalEvents.append(manualEventCreation())
+            
+            myStr = input("\nwant to add new event (Y|N): ")
+            print("\n\n\n")
+            theCondition = lower(myStr[0]) == 'y'
+
+    elif (anInput == "2"):
+        autoConverter()
+
+
+
+main()
+
